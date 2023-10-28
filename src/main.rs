980f165131cd9f5da2f7f59a92719ef9e13d5d59
Chris Baum <crb89101@gmail.com>
2023-10-27 22:05:43 -0400

    added Asset structure

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,11 @@ use rocket::{serde::json::Json, State};
 use rocket::fairing::AdHoc;
 use rocket::tokio::sync::RwLock;
 use serde::{Deserialize, Serialize};
+
+struct Asset {
+    id: u64,
+    asset_code: String,
+    description: String,
+    location: String,
+    cost: u16,
+}
