6ec55ece25016cc59a812b368027068c2c2ee470
Chris Baum <crb89101@gmail.com>
2023-10-27 22:00:17 -0400

    added rocket crate and way to serialize JSON

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate rocket;
+
+use rocket::{serde::json::Json, State};
+use rocket::fairing::AdHoc;
+use rocket::tokio::sync::RwLock;
+use serde::{Deserialize, Serialize};
